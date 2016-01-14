b265fe9030baa5b18ef9c3493665478f49a2222c
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-01-13 23:25:06 -0500

    step 10

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is Josh'
 
-print( hello_world() + 'hector' )
+print( hello_world() + 'Josh' )
